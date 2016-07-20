081a166fd4a964d2caf070ef642b12989b08040f
Wojciech Langiewicz <wlangiewicz@gmail.com>
2016-07-20 12:20:29 +0200

    cleanup

--- src/main/scala/com/lonelyplanet/util/logging/StdOutFilter.scala
+++ src/main/scala/com/lonelyplanet/util/logging/StdOutFilter.scala
@@ -4,16 +4,16 @@ import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.spi.LoggingEvent
 import ch.qos.logback.core.spi.FilterReply
 
-class StdOutFilter extends ch.qos.logback.core.filter.AbstractMatcherFilter{
+class StdOutFilter extends ch.qos.logback.core.filter.AbstractMatcherFilter {
   private val allowed = Set(Level.TRACE, Level.DEBUG, Level.INFO)
 
   override def decide(event: Nothing): FilterReply = {
-    if (!isStarted){
+    if (!isStarted) {
       FilterReply.NEUTRAL
     } else {
       val loggingEvent: LoggingEvent = event.asInstanceOf[LoggingEvent]
 
-      if (allowed.contains(loggingEvent.getLevel)){
+      if (allowed.contains(loggingEvent.getLevel)) {
         FilterReply.NEUTRAL
       } else {
         FilterReply.DENY
