a41192707973421655db13e4bd3c35407210bb9f
Todor Kazakov <todd.kazakov@gmail.com>
2017-12-20 14:50:30 +0200

    Add url and component to airbrake notices

---
+++ src/main/scala/com/lonelyplanet/util/AirbrakeNoticeWithUrlBuilder.scala
@@ -0,0 +1,16 @@
+package com.lonelyplanet.util
+
+import airbrake.{AirbrakeNotice, AirbrakeNoticeBuilder}
+
+class AirbrakeNoticeWithUrlBuilder(apiKey: String, error: Throwable, environment: String)
+  extends AirbrakeNoticeBuilder(apiKey, error, environment) {
+
+  def withUrl(url: String, component: String): AirbrakeNoticeWithUrlBuilder = {
+    setRequest(url, component)
+    this
+  }
+
+  def build(): AirbrakeNotice = {
+    newNotice()
+  }
+}
